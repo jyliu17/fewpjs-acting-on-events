a602fcd5ef1964afd3a79d01227b1a7a27cba78d
jyliu17 <jyliu17@gmail.com>
2020-12-14 23:51:56 -0500

    commit tree 35b37d10840c7611bbd3eacbb398fe65919b6cd0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "red"
+
+console.log(dodger.style.left)
+console.log(dodger.style.bottom)
+
+dodger.style.bottom = "100px"
+dodger.style.left = "100px"
