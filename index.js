8867966391a3914fd670649c79c8fd372ffb5854
jyliu17 <jyliu17@gmail.com>
2021-01-13 20:39:03 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "red"
+
+console.log(dodger.style.left)
+console.log(dodger.style.bottom)
+
+dodger.style.top = "300px"
+dodger.style.left = "200px"
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+
+
+document.addEventListener("keydown", function(evt) {
+    console.log(evt.key);
+    if (evt.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+    else if (evt.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  });
+
+  
